c4e6c6a7cf99a717c62dfee2e00825a47dc644eb
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-08-28 23:36:26 -0500

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return ( 
+    <footer>
+      <small>Coded by <a href="https://twitter.com/ichiklaus" target="_blank" rel="noopener noreferrer">@ichiklaus</a></small>
+    </footer>
+  );
+}
+ 
+export default Footer;
