daf75ead88785717bc3f0533d252d879412d6528
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-08-30 15:09:35 -0500

    Use forms

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Form from './components/Form';
+import ImageUploader from './components/ImageUploader';
 import './App.css';
 
 function App() {
+  const [urlsObject, setUrlsObject] = useState({});
+
   return (
     <div className="App">
       <Header />
       <main>
-        <Form />
+        <div className='product-registration-panel'>
+          <Form urlsObject={urlsObject} setUrlsObject={setUrlsObject} />
+          <ImageUploader setUrlsObject={setUrlsObject} />
+        </div>
       </main>
       <Footer />
     </div>
