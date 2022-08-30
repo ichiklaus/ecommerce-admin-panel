a31a159cdef42fae9956c0892c6887665a2cdc11
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-08-30 15:09:10 -0500

    Create the file uploader form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,9 @@ import { useForm } from 'react-hook-form';
 
 import './Form.css';
 
-const Form = () => {
+const API_URL = `http://127.0.0.1:5000/api/ecommerce-articles/`;
+
+const Form = ({ urlsObject, setUrlsObject }) => {
   const {
     register,
     handleSubmit,
@@ -12,11 +14,34 @@ const Form = () => {
   } = useForm();
 
   const onSubmit = async (data) => {
+    if (data['article-discount'] === '') data['article-discount'] = '0';
+    if (data['article-size'] === '') data['article-size'] = 'Not specified.';
+    // Adds the absolute path of the images uploaded to the body of JSON
+    data['article-images-path'] = urlsObject;
+    console.log(urlsObject);
     console.log(JSON.stringify(data));
+
+    // TODO: Image upload code
+
+    try {
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+      // If response was ok, clear the Urls Object
+      if (response.ok) setUrlsObject(() => ({}));
+    } catch (error) {
+      console.log(error.response);
+    }
+
     // TODO: FIX AXIOS POST REQUEST
     // try {
     //   const response = await axios.post(
-    //     'http://127.0.0.1:5000/api/ecommerce-products/',
+    //     'http://127.0.0.1:5000/api/ecommerce-articles/',
     //     JSON.stringify(data),
     //     {
     //       headers: { Accept: 'application/json', 'Content-Type': 'text/json' },
@@ -26,44 +51,29 @@ const Form = () => {
     // } catch (error) {
     //   console.log(error.response);
     // }
-
-    fetch('http://127.0.0.1:5000/api/ecommerce-products/', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-
-      //make sure to serialize your JSON body
-      body: JSON.stringify(data),
-    }).then((response) => {
-      //do something awesome that makes the world a better place
-      console.log("data uploaded: " + response.data)
-    })
-    .catch(error => console.log(error.response));
   };
   const handleError = (errors) => {};
 
   const registerValidation = {
-    'product-brand': { required: "The product brand's name is required." },
-    'product-name': { required: "The product's name is required." },
-    'product-description': { required: 'A description is required.' },
-    'product-price': {
+    'article-brand': { required: "Article brand's name is required." },
+    'article-name': { required: "Article's name is required." },
+    'article-number': {
+      required: "Article's number is required.",
+      pattern: {
+        value: /^[0-9]+$/g,
+        message: 'Invalid format. Only numbers.',
+      },
+    },
+    'article-description': { required: 'A description is required.' },
+    'article-composition': { required: "Article's composition is required." },
+    'article-retail-price': {
       required: 'This field is required.',
       pattern: {
         value: /^[+-]?([0-9]*[.])?[0-9]+$/g,
         message: 'Invalid value.',
       },
     },
-    'product-discount': {
-      required: 'This field is required.',
-      pattern: {
-        value: /^[+-]?([0-9]*[.])?[0-9]+$/g,
-        message: 'Invalid value.',
-      },
-    },
-    'product-retail-price': {
-      required: 'This field is required.',
+    'article-discount': {
       pattern: {
         value: /^[+-]?([0-9]*[.])?[0-9]+$/g,
         message: 'Invalid value.',
@@ -71,105 +81,131 @@ const Form = () => {
     },
   };
 
-  // TODO: ADD PRODUCT COMPOSITION FIELD
+  // TODO: ADD article COMPOSITION FIELD
   return (
-    <div className="Form">
+    <div id="Form">
       <form onSubmit={handleSubmit(onSubmit, handleError)}>
-        <div id="product-main-info">
+        <div id="article-main-info">
           <div className="col-2">
-            <div id="product-brand-field" className="field-block">
-              <label htmlFor="product-brand">Brand</label>
+            <div id="article-brand-field" className="field-block">
+              <label htmlFor="article-brand">Brand</label>
               <input
-                name="product-brand"
+                name="article-brand"
                 type="text"
                 placeholder="e.g. sneaker company"
                 {...register(
-                  'product-brand',
-                  registerValidation['product-brand'],
+                  'article-brand',
+                  registerValidation['article-brand'],
                 )}
               />
-              <small>
-                {errors?.['product-brand'] && errors['product-brand'].message}
+              <small className="error-message">
+                {errors?.['article-brand'] && errors['article-brand'].message}
               </small>
             </div>
-            <div id="product-name-field" className="field-block">
-              <label htmlFor="product-name">Name</label>
+            <div id="article-name-field" className="field-block">
+              <label htmlFor="article-name">Name</label>
               <input
-                name="product-name"
+                name="article-name"
                 type="text"
                 placeholder="e.g. fall limited edition sneakers"
                 {...register(
-                  'product-name',
-                  registerValidation['product-name'],
+                  'article-name',
+                  registerValidation['article-name'],
                 )}
               />
-              <small>
-                {errors?.['product-name'] && errors['product-name'].message}
+              <small className="error-message">
+                {errors?.['article-name'] && errors['article-name'].message}
               </small>
             </div>
           </div>
-          <div id="product-description-field" className="field-block full">
-            <label htmlFor="product-description">Description</label>
+          <div id="article-number-field" className="field-block full">
+            <label htmlFor="article-number">Article No.</label>
             <input
-              name="product-description"
-              type="area"
-              placeholder="e.g. These low-profile sneakers are your perfect casual wear companion..."
+              name="article-number"
+              type="text"
+              placeholder="e.g. 1106189001"
               {...register(
-                'product-description',
-                registerValidation['product-description'],
+                'article-number',
+                registerValidation['article-number'],
               )}
             />
-            <small>
-              {errors?.['product-description'] &&
-                errors['product-description'].message}
+            <small className="error-message">
+              {errors?.['article-number'] && errors['article-number'].message}
             </small>
           </div>
+          <div id="article-description-field" className="field-block full">
+            <label htmlFor="article-description">Description</label>
+            <input
+              name="article-description"
+              type="textarea"
+              rows="10"
+              placeholder="e.g. These low-profile sneakers are your perfect casual wear companion..."
+              {...register(
+                'article-description',
+                registerValidation['article-description'],
+              )}
+            />
+            <small className="error-message">
+              {errors?.['article-description'] &&
+                errors['article-description'].message}
+            </small>
+          </div>
+          <div id="article-composition-field" className="field-block full">
+            <label htmlFor="article-composition">Composition</label>
+            <input
+              name="article-composition"
+              type="text"
+              placeholder="e.g. Shell: Cotton 98%, Spandex 2%; Pocket lining: Cotton 100% (separate by semicolon)."
+              {...register(
+                'article-composition',
+                registerValidation['article-composition'],
+              )}
+            />
+            <small className="error-message">
+              {errors?.['article-composition'] &&
+                errors['article-composition'].message}
+            </small>
+          </div>
+          <div id="article-size-field" className="field-block full">
+            <label htmlFor="article-size">Article's size</label>
+            <input
+              name="article-size"
+              type="text"
+              placeholder="e.g. Sole: 35 EUR"
+              {...register('article-size')}
+            />
+          </div>
         </div>
-        <div id="product-price-info">
+        <div id="article-price-info">
           <div className="col-3">
-            <div id="product-price-field" className="field-block">
+            <div id="article-retail-price-field" className="field-block">
               <input
-                name="product-price"
-                type="text"
-                placeholder="Price"
-                inputMode="numeric"
-                {...register(
-                  'product-price',
-                  registerValidation['product-price'],
-                )}
-              />
-              <small>
-                {errors?.['product-price'] && errors['product-price'].message}
-              </small>
-            </div>
-            <div id="product-discount-field" className="field-block">
-              <input
-                name="product-discount"
-                type="text"
-                placeholder="Discount"
-                {...register(
-                  'product-discount',
-                  registerValidation['product-discount'],
-                )}
-              />
-              <small>
-                {errors?.['product-discount'] &&
-                  errors['product-discount'].message}
-              </small>
-            </div>
-            <div id="product-retail-price-field" className="field-block">
-              <input
-                name="product-retail-price"
+                name="article-retail-price"
                 type="text"
                 placeholder="Retail Price"
                 {...register(
-                  'product-retail-price',
-                  registerValidation['product-retail-price'],
+                  'article-retail-price',
+                  registerValidation['article-retail-price'],
                 )}
               />
-              <small>
-                {errors?.['product-retail-price'] &&
-                  errors['product-retail-price'].message}
+              <small className="error-message">
+                {errors?.['article-retail-price'] &&
+                  errors['article-retail-price'].message}
+              </small>
+            </div>
+            <div id="article-discount-field" className="field-block">
+              <input
+                name="article-discount"
+                type="text"
+                placeholder="Discount"
+                {...register(
+                  'article-discount',
+                  registerValidation['article-discount'],
+                )}
+              />
+              <small className="error-message">
+                {errors?.['article-discount'] &&
+                  errors['article-discount'].message}
               </small>
             </div>
           </div>
