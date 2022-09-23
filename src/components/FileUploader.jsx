fec1dcc01b00f2ce3e4b7992999cd2644185faff
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-09-23 14:40:27 -0500

    Create files uploader component

---
+++ src/components/FileUploader.jsx
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  storage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from '../firebase';
+import ImageGallery from './ImageGallery';
+import { handleUploadFiles } from '../utils';
+
+import styles from './FileUploader.module.css';
+
+export default function FileUploader({ setUrlsObject, popup, isPopup }) {
+  const [imageArray, setImageArray] = useState([]);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    imageArray.map((imageBlob) => {
+      let imageName = imageBlob.name;
+      const imagesRef = ref(storage, `article-images/${imageName}`);
+      // Upload Task
+      const uploadTask = uploadBytesResumable(imagesRef, imageBlob);
+
+      return uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        },
+        (error) => {
+          switch (error.code) {
+            case 'storage/unauthorized':
+              // User doesn't have permission to access the object
+              console.log("User doesn't have permission to access the object");
+              break;
+            case 'storage/unknown':
+              // Unknown error occurred, inspect error.serverResponse
+              console.log(
+                'Unknown error occurred, inspect error.serverResponse',
+              );
+              break;
+            default:
+              console.log('Unknown');
+          }
+        },
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+            setUrlsObject((prevState) => ({
+              ...prevState,
+              [imageName]: downloadURL,
+            }));
+          });
+        },
+      );
+    });
+  };
+
+  function nextStep() {}
+  function cancel() {
+    // console.log(popup);
+    isPopup(() => !popup);
+  }
+
+  return (
+    <div
+      className={`${styles.modal} ${popup ? styles.visible : styles.invisible}`}
+    >
+      <div className={styles['modal-content']}>
+        <span className={styles.close} onClick={cancel}>
+          &times;
+        </span>
+        <div className={styles['file-uploader-container']}>
+          <ImageGallery imageArray={imageArray} setImageArray={setImageArray} />
+          <div className={styles['action-container']}>
+            <div className={styles[`input-file-wrapper`]}>
+              <div>
+                <label className={styles['button']} htmlFor="upload-picture">
+                  Select images
+                </label>
+                <input
+                  multiple
+                  accept="image/png, image/jpg, image/jpeg, image/webp"
+                  type="file"
+                  name="upload-picture"
+                  id="upload-picture"
+                  {...register('upload-picture', {
+                    // onChange: (e) => {
+                    //   setImageArray((prev) => [...prev, e.target.files[0]]);
+                    // },
+                    onChange: (event) => {
+                      const chosenFiles = Array.prototype.slice.call(
+                        event.target.files,
+                      );
+                      handleUploadFiles(chosenFiles, [...imageArray], setImageArray);
+                      event.target.value = '';
+                    },
+                  })}
+                />
+              </div>
+            </div>
+            <div className={styles['action-wrapper']}>
+              <span
+                className={`${styles['button']} ${styles['continue']}`}
+                onClick={nextStep}
+              >
+                Continue
+              </span>
+              <span
+                className={`${styles['button']} ${styles['cancel']}`}
+                onClick={cancel}
+              >
+                Cancel
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
