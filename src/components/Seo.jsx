b4becacdbce30ba7df7d4ac0a99cfbac6795f5f1
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-09-23 14:38:14 -0500

    Add seo component

---
+++ src/components/Seo.jsx
@@ -0,0 +1,17 @@
+
+export default function Seo({ description, title, sitetitle }) {
+  return (
+    <>
+      <title>{`${title} | ${sitetitle}`}</title>
+      <meta name="description" content={description} />
+      {/* <meta property="og:type" content="website" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:creator" content={config.social.twitter} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} /> */}
+    </>
+  )
+}
