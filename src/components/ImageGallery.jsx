4fb4f86241514259370c33b2eef29b50da45a3b4
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-09-23 14:43:41 -0500

    Create an image gallery component

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,25 +1,66 @@
 import React from 'react';
+import { handleUploadFiles } from '../utils';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-import './ImageGallery.css'
+import styles from './ImageGallery.module.css';
 
-const ImageGallery = ({ imageArray }) => {
+export default function ImageGallery({ imageArray, setImageArray }) {
+  // console.log('in gallery', imageArray);
+  const removeFileFromArray = (referenceName) => {
+    let fileIndex = -1;
+    if (referenceName !== '') {
+      // If referenceName is not empty then get the index of that reference in the array
+      fileIndex = imageArray.findIndex((file) => file.name === referenceName);
+      // console.log('index of referenced image', fileIndex);
+      // console.log('reference', referenceName);
+    }
+
+    // Copy the new array after slicing the original array
+    const slicedArray = [
+      ...imageArray.slice(0, fileIndex),
+      ...imageArray.slice(fileIndex + 1, imageArray.length),
+    ];
+
+    // Reuploads the new array
+    handleUploadFiles(slicedArray, [], setImageArray);
+  };
+
   return (
-    <div id="ImageGallery">
-      {imageArray.map((previewImage) => {
-        // TODO: Theres a bug that renders twice this log
-        // console.log('data in the map', previewImage && previewImage.name);
-        return (
-          <img
-            key={previewImage && previewImage.name}
-            src={previewImage && URL.createObjectURL(previewImage)}
-            alt={previewImage && previewImage.name}
-            width="100%"
-            height="auto"
-          />
-        );
-      })}
+    <div id="ImageGallery" className={styles[`image-gallery`]}>
+      {imageArray &&
+        imageArray.map((previewImage, index) => {
+          // TODO: Theres a bug that renders twice this log
+          // console.log('data in the map', previewImage);
+          // console.log(index);
+          return (
+            <div
+              key={previewImage && previewImage.name}
+              className={styles['image-wrapper']}
+            >
+              <img
+                // key={previewImage && previewImage.name}
+                src={previewImage && URL.createObjectURL(previewImage)}
+                alt={previewImage && previewImage.name}
+                width="100%"
+                height="auto"
+              />
+              <div className={styles['image-overlay']}>
+                <span
+                  // key={previewImage && previewImage.name}
+                  className={`${styles['image-icon']} ${styles['shake']}`}
+                  onClick={() => {
+                    setTimeout(() => {
+                      removeFileFromArray(previewImage.name);
+                    }, 100);
+                  }}
+                >
+                  <FontAwesomeIcon id={styles['trash-icon']} icon={faTrash} color="white" />
+                </span>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
-};
-
-export default ImageGallery;
+}
