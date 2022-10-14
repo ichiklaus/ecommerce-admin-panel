92be500776a45d8e83403bb9ce53e02cb0e2fe69
Yeraldo Nicolás Moreira <nicola1994n@gmail.com>
2022-10-14 14:50:16 -0500

    Add login to home

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
-import Form from '../src/components/Form';
-import ImageUploader from '../src/components/ImageUploader';
+
+import Login from '../src/components/Login';
 
 export default function Home() {
-  // const [urlsObject, setUrlsObject] = useState({});
 
   return (
     <div className="App">
       <div className='product-registration-panel'>
-        <Form
-        // urlsObject={urlsObject} setUrlsObject={setUrlsObject}
-        />
-        {/* <ImageUploader setUrlsObject={setUrlsObject} /> */}
+        <Login />
       </div>
     </div>
   );
